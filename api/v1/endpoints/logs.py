86c27caa5ba405246a0e48fface274d039dd542e
R-dVL <rauldel.valle.lima@gmail.com>
2024-04-30 11:10:53 +0200

    Add total statistics endpoint

--- api/v1/endpoints/logs.py
+++ api/v1/endpoints/logs.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Query
 from fastapi.responses import FileResponse
-import subprocess
 from datetime import date
 from pathlib import Path
+import glob
+import re
+from collections import Counter
 
 router = APIRouter()
 
@@ -18,3 +20,21 @@ def get_log_content(
         return FileResponse(log_file_path, media_type="text/plain")
     else:
         return {"error": "Log doesn't exists."}
+
+@router.get("/total")
+def get_log_content(playbook: str = Query(..., description="Playbook name")):
+    path = Path(f"/logs/{playbook}/")
+
+    if path.exists():
+        # Obtener todas las subcarpetas en la carpeta
+        subfolders = [f.name for f in path.iterdir() if f.is_dir()]
+
+        # Extraer el año y el mes de cada nombre de subcarpeta
+        dates = [re.search(r"\d{4}-\d{2}", folder).group(0) for folder in subfolders]
+
+        # Contar las ocurrencias de cada fecha
+        date_counts = dict(Counter(dates))
+
+        return date_counts
+    else:
+        return {"error": "Path doesn't exists."}
