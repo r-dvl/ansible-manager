3b227b1141d4abe1c31aaf3af7622406dac96aed
R-dVL <rauldel.valle.lima@gmail.com>
2024-04-30 14:47:08 +0200

    Real data in latest executions

--- ui/src/sections/home/view/home-view.jsx
+++ ui/src/sections/home/view/home-view.jsx
@@ -15,16 +15,43 @@ import HomeExecutionStatistics from '../home-execution-statistics';
 
 export default function HomeView() {
   const [executionStatistics, setExecutionStatistics] = useState({});
+  const [latestExecutions, setLatestExecutions] = useState([]);
+  const [statisticsError, setStatisticsError] = useState(null);
+  const [executionsError, setExecutionsError] = useState(null);
 
   useEffect(() => {
-    fetch(`http://192.168.1.39:8080/v1/logs/execution-statistics?year=2024`)
-      .then(response => response.json())
-      .then(data => setExecutionStatistics(data));
+    fetch('http://192.168.1.39:8080/v1/logs/execution-statistics?year=2024')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error fetching execution statistics');
+        }
+        return response.json();
+      })
+      .then(data => setExecutionStatistics(data))
+      .catch(error => setStatisticsError(error.message));
+
+    fetch('http://192.168.1.39:8080/v1/logs/last-executions')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error fetching latest executions');
+        }
+        return response.json();
+      })
+      .then(data => setLatestExecutions(data))
+      .catch(error => setExecutionsError(error.message));
   }, []);
 
-  if (!executionStatistics.global) {
-    return <div>Cargando...</div>;
+  if (statisticsError) {
+    return <div>Error fetching execution statistics: {statisticsError}</div>;
   }
+
+  if (executionsError) {
+    return <div>Error fetching latest executions: {executionsError}</div>;
+  }
+
+  if (!executionStatistics.global) {
+    return <div>Loading...</div>;
+  };
 
   const playbookSeries = Object.entries(executionStatistics)
     .filter(([playbook]) => playbook !== 'global')
@@ -131,13 +158,17 @@ export default function HomeView() {
         <Grid xs={12} md={6} lg={8}>
           <HomeLatestExecutions
             title="Latest Executions"
-            list={[...Array(5)].map((_, index) => ({
-              id: "001",
-              title: "Backup",
-              description: "Success",
-              image: `/assets/images/covers/cover_${index + 1}.jpg`,
-              postedAt: faker.date.recent(),
-            }))}
+            list={latestExecutions.map((execution, index) => {
+              // Convertir el string datetime a un objeto Date
+              const dateObject = new Date(execution.datetime);
+              return {
+                id: `${execution.playbook}-${execution.datetime}-${index}`,
+                title: execution.playbook,
+                description: execution.status,
+                image: `/assets/icons/jobs/${execution.status}.png`,
+                postedAt: dateObject,
+              };
+            })}
           />
         </Grid>
 
