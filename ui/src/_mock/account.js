ff5170c5f97ef0e3bb41e53f8687bf67c755e2f2
R-dVL <rauldel.valle.lima@gmail.com>
2024-05-04 23:33:01 +0200

    Temporal hardcoded user info

--- ui/src/_mock/account.js
+++ ui/src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 export const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
+  displayName: 'R-dVL',
+  email: 'rauldel.valle.lima@gmail.com',
   photoURL: '/assets/images/avatars/avatar.svg',
 };
