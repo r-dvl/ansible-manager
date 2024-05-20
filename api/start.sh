c741e5cd5b60c3fd838e09a3fe791ccae0a915fc
Raúl Del Valle Lima <rauldel.valle.lima@gmail.com>
2024-05-20 13:51:23 +0200

    Hotfix/cron-service

--- api/start.sh
+++ api/start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Start cron service
+# Set Time Zone
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+# Install and start cron service
+chmod 0644 /etc/cron.d/ansible
+crontab /etc/cron.d/ansible
 service cron start
 
 # Start your application
