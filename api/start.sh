553ebd4e99ccc76617c86236e5d14300d351c5ad
R-dVL <rauldel.valle.lima@gmail.com>
2024-06-01 12:45:17 +0200

    Hotfix: ansible path

--- api/start.sh
+++ api/start.sh
@@ -3,10 +3,20 @@
 # Set Time Zone
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-# Install and start cron service
+# Ensure PATH includes the directory for ansible-playbook
+export PATH=$PATH:/usr/local/bin
+
+# Set permissions for the cron file (copied from a volume)
 chmod 0644 /etc/cron.d/ansible
+
+# Apply the cron job
 crontab /etc/cron.d/ansible
+
+# Start the cron service
 service cron start
+
+# Create the log file to be able to run tail
+touch /var/log/cron.log
 
 # Start application
 python3 ./main.py
